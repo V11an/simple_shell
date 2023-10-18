6667df16f7316e1349cc5609afc73fc7657f8dc4
V11an <allankertich@gmail.com>
2023-10-18 07:49:11 +0000

    Handle path

--- simple_shell.h
+++ simple_shell.h
@@ -20,5 +20,6 @@ char *read_line(void);
 char **parse_line(char *line);
 int execute_command(char **args);
 void free_memory(char *line, char **args);
+char *find_command_in_path(char *command);
 
 #endif /* SIMPLE_SHELL_H */
